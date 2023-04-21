1e2a2a2c418dae1a126b372bd21be75f8647f26f
ShuranovaNI <ni.shuranova@gmail.com>
2023-04-21 21:18:40 +0300

    commit test fails

--- src/test/java/ru/netology/WebInterfaceTest.java
+++ src/test/java/ru/netology/WebInterfaceTest.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +17,7 @@ class CallbackTest {
 
     @BeforeAll
     static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+      System.setProperty("webdriver.chrome.driver", "/driver/chromedriver.exe");
     }
 
     @BeforeEach
@@ -28,7 +27,7 @@ class CallbackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+
 
 
     }
@@ -42,11 +41,12 @@ class CallbackTest {
     @Test
     void shouldTestSomething() {
 
+        driver.get("http://localhost:9999");
 
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id=name]input")).sendKeys("Василий Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone]input")).sendKeys("+79277900546");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("(button.button)")).click();
+        driver.findElement(By.cssSelector("[button.button]")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected,actual);
